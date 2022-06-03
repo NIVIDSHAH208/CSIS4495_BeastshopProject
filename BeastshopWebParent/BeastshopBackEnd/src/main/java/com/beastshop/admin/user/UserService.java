ccee39c4c0c506b5dd55cf13782f37126a54048b
Nivid Shah <shahn8@student.douglascollege.ca>
2022-06-03 10:47:01 -0700

    New user form created with validations
    New users can be saved with dynamic page references using thymeleaf syntax

--- BeastshopWebParent/BeastshopBackEnd/src/main/java/com/beastshop/admin/user/UserService.java
+++ BeastshopWebParent/BeastshopBackEnd/src/main/java/com/beastshop/admin/user/UserService.java
@@ -5,16 +5,31 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.beastshop.common.entity.Role;
 import com.beastshop.common.entity.User;
 
 @Service
 public class UserService {
 	
 	@Autowired
-	private UserRepository repo;
+	private UserRepository userRepo;
+	
+	@Autowired
+	private RoleRepository roleRepo;
 	
 	//Method to return the list of users
 	public List<User> listAll(){
-		return (List<User>)repo.findAll();
+		return (List<User>)userRepo.findAll();
 	}
+	
+	//Method to return list of roles object from the database
+	public List<Role> listAllRoles(){
+		return (List<Role>)roleRepo.findAll();
+	}
+
+	public void save(User user) {
+		userRepo.save(user);
+		
+	}
+	
 }
