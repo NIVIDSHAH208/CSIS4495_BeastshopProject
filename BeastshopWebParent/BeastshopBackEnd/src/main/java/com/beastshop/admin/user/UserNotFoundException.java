1d309a0cef896129adf390365842b8c933579126
Nivid Shah <shahn8@student.douglascollege.ca>
2022-06-04 20:57:16 -0700

    User update form completed
    User update with tests

---
+++ BeastshopWebParent/BeastshopBackEnd/src/main/java/com/beastshop/admin/user/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package com.beastshop.admin.user;
+
+public class UserNotFoundException extends Exception {
+
+	//Creating the custom exception to handle the user not found exception
+	public UserNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
--- BeastshopWebParent/BeastshopBackEnd/src/main/java/com/beastshop/admin/user/UserService.java
+++ BeastshopWebParent/BeastshopBackEnd/src/main/java/com/beastshop/admin/user/UserService.java
@@ -1,6 +1,7 @@
 package com.beastshop.admin.user;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,43 +12,82 @@ import com.beastshop.common.entity.User;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository userRepo;
-	
+
 	@Autowired
 	private RoleRepository roleRepo;
-	
+
 	@Autowired
 	private PasswordEncoder passwordEncoder;
-	
-	//Method to return the list of users
-	public List<User> listAll(){
-		return (List<User>)userRepo.findAll();
+
+	// Method to return the list of users
+	public List<User> listAll() {
+		return (List<User>) userRepo.findAll();
 	}
-	
-	//Method to return list of roles object from the database
-	public List<Role> listAllRoles(){
-		return (List<Role>)roleRepo.findAll();
+
+	// Method to return list of roles object from the database
+	public List<Role> listAllRoles() {
+		return (List<Role>) roleRepo.findAll();
 	}
 
 	public void save(User user) {
-		encodePassword(user);
+
+		boolean isUpdatingUser = (user.getId() != null);
+		if (isUpdatingUser) {
+			User existingUser = userRepo.findById(user.getId()).get();
+
+			if (user.getPassword().isEmpty()) {
+				user.setPassword(existingUser.getPassword());
+			} else {
+				encodePassword(user);
+			}
+		} else {
+			encodePassword(user);
+		}
 		userRepo.save(user);
-		
+
 	}
-	
-	//method to encode the user password
+
+	// method to encode the user password
 	private void encodePassword(User user) {
 		String encodedPasswordString = passwordEncoder.encode(user.getPassword());
 		user.setPassword(encodedPasswordString);
 	}
-	
-	//method to check the uniqueness of the user
-	public boolean isEmailUnique(String email) {
-		//if user exists then it will return false, otherwise it will return true
-		User userByEmail =userRepo.getUserByEmail(email);
-		return userByEmail==null;
+
+	// method to check the uniqueness of the user
+	public boolean isEmailUnique(Integer id, String email) {
+		// if user exists then it will return false, otherwise it will return true
+		User userByEmail = userRepo.getUserByEmail(email);
+		if (userByEmail == null) {
+			return true;
+		}
+		boolean isCreatingNew = (id == null);
+		// if we are creating new user, if email is used then --> false
+		if (isCreatingNew) {
+			if (userByEmail != null) {
+				return false;
+			}
+		}
+		// for the edited user, if id is not the same that means it is not the same user
+		else {
+			if (userByEmail.getId() != id) {
+				return false;
+			}
+		}
+
+		return true;
 	}
-	
+
+	// Method to get the user by id in order to update/delete
+	public User getById(Integer id) throws UserNotFoundException {
+
+		try {
+			return userRepo.findById(id).get();
+		} catch (NoSuchElementException ex) {
+			throw new UserNotFoundException("Could not find any user with ID " + id);
+		}
+
+	}
 }
