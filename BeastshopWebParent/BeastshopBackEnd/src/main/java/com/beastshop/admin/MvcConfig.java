f2c32028076d98f8fab90ade1ea37f7ed2c11d12
Nivid Shah <shahn8@student.douglascollege.ca>
2022-06-21 11:59:35 -0700

    Method for MVCConfig and Responsive brands listing page

--- BeastshopWebParent/BeastshopBackEnd/src/main/java/com/beastshop/admin/MvcConfig.java
+++ BeastshopWebParent/BeastshopBackEnd/src/main/java/com/beastshop/admin/MvcConfig.java
@@ -11,31 +11,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
 	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		String dirName= "user-photos";
-		Path userPhotosDirPath = Paths.get(dirName);
-		String userPhotosPath=userPhotosDirPath.toFile().getAbsolutePath();
-		
-		//method below expose the absolute path of user photos directory to the end user
-		registry.addResourceHandler("/"+dirName+"/**").addResourceLocations("file:/"+userPhotosPath+"/");
-		
-		
-		String categoryImagesDirName= "../category-images";
-		Path categoryImageDir = Paths.get(categoryImagesDirName);
-		String categoryImagesPath=categoryImageDir.toFile().getAbsolutePath();
-		
-		//method below expose the absolute path of user photos directory to the end user
-		registry.addResourceHandler("/category-images/**")
-			.addResourceLocations("file:/"+categoryImagesPath+"/");
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {		
+		exposeDirectory("user-photos", registry);
+		exposeDirectory("../category-images",registry);
+		exposeDirectory("../brand-logos",registry);
+	}
+	
+	private void exposeDirectory(String pathPattern, ResourceHandlerRegistry registry){
+		Path path = Paths.get(pathPattern);
+		String absolutePath=path.toFile().getAbsolutePath();
+		String logicalPath = pathPattern.replace("../", "")+"/**";
 		
 		
-		String brandLogosDirName= "../brand-logos";
-		Path brandLogosDir = Paths.get(brandLogosDirName);
-		String brandLogosPath=brandLogosDir.toFile().getAbsolutePath();
-		
-		//method below expose the absolute path of user photos directory to the end user
-		registry.addResourceHandler("/brand-logos/**")
-			.addResourceLocations("file:/"+brandLogosPath+"/");
+		registry.addResourceHandler(logicalPath)
+		.addResourceLocations("file:/"+absolutePath+"/");
+
 	}
 	
 }
