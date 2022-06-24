2c4456bc9d8f8aa1a8af015cb623382713977d53
Nivid Shah <shahn8@student.douglascollege.ca>
2022-06-24 09:51:17 -0700

    Check uniqueness of product name, update enable, delete product

--- BeastshopWebParent/BeastshopBackEnd/src/main/java/com/beastshop/admin/product/ProductController.java
+++ BeastshopWebParent/BeastshopBackEnd/src/main/java/com/beastshop/admin/product/ProductController.java
@@ -6,10 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.beastshop.admin.FileUploadUtil;
 import com.beastshop.admin.brand.BrandService;
+import com.beastshop.admin.category.CategoryNotFoundException;
 import com.beastshop.common.entity.Brand;
 import com.beastshop.common.entity.Product;
 
@@ -46,5 +49,31 @@ public class ProductController {
 		productService.save(product);
 		redirectAttributes.addFlashAttribute("message","The product has been saved successfully");
 		return "redirect:/products";
+	}
+	
+	@GetMapping("/products/{id}/enabled/{status}")
+	public String updateProductEnabledStatus(@PathVariable("id") Integer id, @PathVariable("status") boolean enabled,
+			RedirectAttributes redirectAttributes) {
+		productService.updateProductEnableStatus(id, enabled);
+		String status = enabled ? "enabled" : "disabled";
+		String message = "The product Id " + id + " has been " + status;
+		redirectAttributes.addFlashAttribute("message", message);
+		return "redirect:/products";
+	}
+	
+	@GetMapping("/products/delete/{id}")
+	public String deleteProduct(@PathVariable(name = "id") Integer id, Model model,
+			RedirectAttributes redirectAttributes) {
+		try {
+			productService.delete(id);
+			
+			redirectAttributes.addFlashAttribute("message",
+					"The product ID " + id + " has been deleted successfully.");
+		} catch (ProductNotFoundException ex) {
+			redirectAttributes.addFlashAttribute("message", ex.getMessage());
+
+		}
+		return "redirect:/products";
+
 	}
 }
---
+++ BeastshopWebParent/BeastshopBackEnd/src/main/java/com/beastshop/admin/product/ProductNotFoundException.java
@@ -0,0 +1,10 @@
+package com.beastshop.admin.product;
+
+public class ProductNotFoundException extends Exception {
+
+	public ProductNotFoundException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+
+}
