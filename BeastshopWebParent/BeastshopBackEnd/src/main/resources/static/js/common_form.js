e024acd829b76302e8c76ebb805c09bfd3e4ba41
Nivid Shah <shahn8@student.douglascollege.ca>
2022-06-24 12:38:38 -0700

    Add extra product Images, product_form.js to add sections dynamically

--- BeastshopWebParent/BeastshopBackEnd/src/main/resources/static/js/common_form.js
+++ BeastshopWebParent/BeastshopBackEnd/src/main/resources/static/js/common_form.js
@@ -1,32 +1,36 @@
-$(document)
-	.ready(
-			function() {
-				$("#buttonCancel").on("click", function() {
-					window.location = moduleURL;
+$(document).ready(function() {
+			$("#buttonCancel").on("click", function() {
+				window.location = moduleURL;
+			});
+
+			$("#fileImage").change(function() {
+				if(!checkFileSize(this)){
+					return;
+				}					
+						showImageThumnail(this);
+
 				});
 
-				$("#fileImage")
-						.change(
-								function() {
-									fileSize = this.files[0].size;
-
-									if (fileSize > MAX_FILE_SIZE) {
-										this
-												.setCustomValidity("You must choose an image less than "+MAX_FILE_SIZE+" bytes");
-										this.reportValidity();
-									} else {
-										this.setCustomValidity("");
-										showImageThumnail(this);
-									}
-
-								})
-
-			});
+		});
 			
 function showModalDialog(title, message) {
 	$("#modalTitle").text(title);
 	$("#modalBody").text(message);
 	$("#modalDialog").modal();
+}
+
+function checkFileSize(fileInput){
+	fileSize = fileInput.files[0].size;
+	
+	if (fileSize > MAX_FILE_SIZE) {
+		fileInput
+				.setCustomValidity("You must choose an image less than "+MAX_FILE_SIZE+" bytes");
+		fileInput.reportValidity();
+		return false;
+	} else {
+		fileInput.setCustomValidity("");
+		return true;
+	}
 }
 
 function showErrorModal(message){
