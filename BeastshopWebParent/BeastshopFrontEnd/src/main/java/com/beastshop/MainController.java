1d66a1b45a0d42c2bc39c8c85121940c6673e2cb
Nivid Shah <shahn8@student.douglascollege.ca>
2022-07-01 12:26:10 -0700

    Shop by Categories Home page & MVC Conf &Web sec. config on front end

--- BeastshopWebParent/BeastshopFrontEnd/src/main/java/com/beastshop/MainController.java
+++ BeastshopWebParent/BeastshopFrontEnd/src/main/java/com/beastshop/MainController.java
@@ -1,12 +1,23 @@
 package com.beastshop;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.beastshop.category.CategoryService;
+import com.beastshop.common.entity.Category;
 
 @Controller
 public class MainController {
+	@Autowired
+	private CategoryService categoryService;
 	@GetMapping("")
-	public String viewHomePage() {
+	public String viewHomePage(Model model) {
+		List<Category> listCategories = categoryService.listNoChildrenCategories();
+		model.addAttribute("listCategories",listCategories);
 		return "index";
 	}
 }
---
+++ BeastshopWebParent/BeastshopFrontEnd/src/test/java/com/beastshop/category/CategoryRepositoryTest.java
@@ -0,0 +1,27 @@
+package com.beastshop.category;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.beastshop.common.entity.Category;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace=Replace.NONE)
+public class CategoryRepositoryTest {
+
+	@Autowired
+	private CategoryRepository repo;
+	
+	@Test
+	public void testListEnabledCategories() {
+		List<Category> categories = repo.findAllEnabled();
+		categories.forEach(category->{
+			System.out.println(category.getName()+" ( "+category.isEnabled()+")");
+		});
+	}
+}
