6c56839e69fd7f7cb801aadd12252064cd44057a
Nivid Shah <shahn8@student.douglascollege.ca>
2022-07-01 12:03:14 -0700

    Beast shop front end home page & custom error pages

--- BeastshopWebParent/BeastshopFrontEnd/src/main/java/com/beastshop/BeastshopFrontEndApplication.java
+++ BeastshopWebParent/BeastshopFrontEnd/src/main/java/com/beastshop/BeastshopFrontEndApplication.java
@@ -1,4 +1,4 @@
-package com.beastshop.site;
+package com.beastshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- BeastshopWebParent/BeastshopFrontEnd/src/main/java/com/beastshop/MainController.java
+++ BeastshopWebParent/BeastshopFrontEnd/src/main/java/com/beastshop/MainController.java
@@ -1,4 +1,4 @@
-package com.beastshop.site;
+package com.beastshop;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
