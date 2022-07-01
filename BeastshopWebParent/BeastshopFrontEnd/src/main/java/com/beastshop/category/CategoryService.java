f6c1c16b8e2c9d9dd729e1024aa5079b0353b405
Nivid Shah <shahn8@student.douglascollege.ca>
2022-07-01 16:10:20 -0700

    Product listing based on category for customers

--- BeastshopWebParent/BeastshopFrontEnd/src/main/java/com/beastshop/category/CategoryService.java
+++ BeastshopWebParent/BeastshopFrontEnd/src/main/java/com/beastshop/category/CategoryService.java
@@ -27,4 +27,23 @@ public class CategoryService {
 		});
 		return listNoChildrenCategories;
 	}
+	//Method to get the category by alias
+	public Category getCategory(String alias) {
+		return repo.findByAliasEnabled(alias);
+	}
+	
+	//Method to list the parent categories
+	public List<Category> getCategoryParents(Category child){
+		List<Category> listParents=new ArrayList<>();
+		Category parent = child.getParent();
+		
+		while(parent!=null) {
+			listParents.add(0,parent);
+			parent=parent.getParent();
+		}
+		listParents.add(child);
+		
+		return listParents;
+	}
+	
 }
