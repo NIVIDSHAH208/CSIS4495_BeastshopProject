4345637c04e8e22662276edbc9a4510a6e9b3b7e
Nivid Shah <shahn8@student.douglascollege.ca>
2022-07-02 23:01:05 -0700

    Currency entity, repository, and unit tests

---
+++ BeastshopWebParent/BeastshopBackEnd/src/main/java/com/beastshop/admin/setting/CurrencyRepository.java
@@ -0,0 +1,9 @@
+package com.beastshop.admin.setting;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.beastshop.common.entity.Currency;
+
+public interface CurrencyRepository extends CrudRepository<Currency, Integer> {
+
+}
---
+++ BeastshopWebParent/BeastshopBackEnd/src/test/java/com/beastshop/admin/setting/CurrencyRepositoryTest.java
@@ -0,0 +1,49 @@
+package com.beastshop.admin.setting;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import com.beastshop.common.entity.Currency;
+import com.beastshop.common.entity.Setting;
+import com.beastshop.common.entity.SettingCategory;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class CurrencyRepositoryTest {
+	@Autowired CurrencyRepository repo;
+	
+	@Test
+	public void testCreateCurrencies() {
+		List<Currency> listCurrencies=Arrays.asList(
+				new Currency("United States Dollars", "$", "USD"),
+				new Currency("British Pound", "£", "GBP"),
+				new Currency("Japanese Yen", "¥", "JPY"),
+				new Currency("Euro", "€", "EUR"),
+				new Currency("Russian Ruble", "₽", "RUB"),
+				new Currency("South Korean Won", "₩", "KRW"),
+				new Currency("Chinese Yuan", "¥", "CNY"),
+				new Currency("Brazilian Real", "R$", "BRL"),
+				new Currency("Australian Dollar", "$", "AUD"),
+				new Currency("Canadian Dollars", "$", "CAD"),
+				new Currency("Vietnamese đồng", "đ", "VND"),
+				new Currency("Indian Rupee", "₹", "INR")
+				);
+		
+		repo.saveAll(listCurrencies);
+		
+		Iterable<Currency> iterable = repo.findAll();
+		
+		assertThat(iterable).size().isEqualTo(12);
+	}
+}
