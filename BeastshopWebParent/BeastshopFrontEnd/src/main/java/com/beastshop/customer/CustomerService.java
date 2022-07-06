571c8314496db8ddb0655665e1da3198b2a09923
Nivid Shah <shahn8@student.douglascollege.ca>
2022-07-06 14:06:34 -0700

    Verify email and enable the customer

--- BeastshopWebParent/BeastshopFrontEnd/src/main/java/com/beastshop/customer/CustomerService.java
+++ BeastshopWebParent/BeastshopFrontEnd/src/main/java/com/beastshop/customer/CustomerService.java
@@ -3,6 +3,8 @@ package com.beastshop.customer;
 import java.util.Date;
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -14,35 +16,35 @@ import com.beastshop.setting.CountryRepository;
 import net.bytebuddy.utility.RandomString;
 
 @Service
+@Transactional
 public class CustomerService {
 	@Autowired
 	private CountryRepository countryRepo;
-	
+
 	@Autowired
 	private CustomerRepository customerRepo;
-	
+
 	@Autowired
 	PasswordEncoder passwordEncoder;
-	
-	public List<Country> listAllCountries(){
+
+	public List<Country> listAllCountries() {
 		List<Country> findAllByOrderByNameAsc = countryRepo.findAllByOrderByNameAsc();
 		return findAllByOrderByNameAsc;
 	}
-	
-	//If customer exists, then  the object will not be null
+
+	// If customer exists, then the object will not be null
 	public boolean isEmailUnique(String email) {
 		Customer customer = customerRepo.findByEmail(email);
-		return customer==null;
+		return customer == null;
 	}
-	
-	
-	//Register customer and encode the password
+
+	// Register customer and encode the password
 	public void registerCustomer(Customer customer) {
 		encodePassword(customer);
 		customer.setEnabled(false);
 		customer.setCreatedTime(new Date());
-		
-		//Randomstring comes in spring bydefault
+
+		// Randomstring comes in spring by default
 		String randomCode = RandomString.make(64);
 		customer.setVerificationCode(randomCode);
 //		System.out.println("Verification code is: "+randomCode);
@@ -53,6 +55,16 @@ public class CustomerService {
 		// TODO Auto-generated method stub
 		String encodedPassword = passwordEncoder.encode(customer.getPassword());
 		customer.setPassword(encodedPassword);
-		
+	}
+
+	// method to verify customer account and enable the customer
+	public boolean verify(String verificationCode) {
+		Customer customer = customerRepo.findByVerificationCode(verificationCode);
+		if(customer==null||customer.isEnabled()) {
+			return false;
+		}else {
+			customerRepo.enable(customer.getId());
+			return true;
+		}
 	}
 }
