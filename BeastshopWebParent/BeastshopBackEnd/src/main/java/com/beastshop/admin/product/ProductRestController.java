ad2245b6cd8eaafdc0e83c0e759634233ba15d83
Nivid Shah <shahn8@student.douglascollege.ca>
2022-07-18 09:35:28 -0700

    Add products to exist. order | Using ajax generate HTML code dynamically

--- BeastshopWebParent/BeastshopBackEnd/src/main/java/com/beastshop/admin/product/ProductRestController.java
+++ BeastshopWebParent/BeastshopBackEnd/src/main/java/com/beastshop/admin/product/ProductRestController.java
@@ -2,8 +2,13 @@ package com.beastshop.admin.product;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.beastshop.common.entity.product.Product;
+import com.beastshop.common.exception.ProductNotFoundException;
 
 @RestController
 public class ProductRestController {
@@ -13,5 +18,12 @@ public class ProductRestController {
 	@PostMapping("/products/check_unique")
 	public String checkUnique(Integer id,  String name) {
 		return service.checkUnique(id, name);
+	}
+	
+	@GetMapping("/products/get/{id}")
+	public ProductDTO getProductInfo(@PathVariable("id") Integer id) throws ProductNotFoundException {
+		Product product = service.get(id);
+		//new lightweight data transfer object 
+		return new ProductDTO(product.getName(),product.getMainImagePath(), product.getDiscountPrice(), product.getCost());
 	}
 }
--- BeastshopWebParent/BeastshopBackEnd/src/main/resources/static/js/order_form_add_product.js
+++ BeastshopWebParent/BeastshopBackEnd/src/main/resources/static/js/order_form_add_product.js
@@ -16,3 +16,136 @@
  		$("#addProductModal").modal();
  	});
  });
+ 
+ function addProduct(productId, productName){
+ 	$("#addProductModal").modal("hide");
+ 	getShippingCost(productId);
+ }
+ 
+ function getShippingCost(productId){
+ 	 selectedCountry = $("#country option:selected")
+ 	 countryId = selectedCountry.val();
+ 	 state = $("#state").val();
+ 	 if(state.length==0){
+ 	 	state=$("#city").val()
+ 	 }
+ 	 
+ 	 requestURL = contextPath +"get_shipping_cost";
+ 	 params = {productId: productId, countryId:countryId, state:state};
+ 	 
+ 	 $.ajax({
+		type: "POST",
+		url: requestURL,
+		beforeSend: function(xhr){
+			xhr.setRequestHeader(csrfHeaderName, csrfValue);
+		},
+		data : params
+	}).done(function(shippingCost){
+		getProductInfo(productId, shippingCost);
+	}).fail(function(err){
+		showWarningModal(err.responseJSON.message);
+		shippingCost=0.0;
+		getProductInfo(productId, shippingCost);
+	}).always(function(){
+		$("#addProductModal").modal("hide");
+	});
+ 
+ }
+ 
+ function getProductInfo(productId, shippingCost){
+ 	requestUrl = contextPath + "products/get/"+productId;
+ 	$.get(requestUrl, function(productJSON){
+ 		//console.log(productJSON)
+ 		productName = productJSON.name;
+ 		mainImagePath = contextPath.substring(0, contextPath.length-1) + productJSON.imagePath;
+ 		productCost = $.number(productJSON.cost, 2);
+ 		productPrice = $.number(productJSON.price, 2);
+ 		htmlCode = generateProductCode(productId, productName, mainImagePath, productCost, productPrice, shippingCost);
+ 		$("#productList").append(htmlCode);
+ 	}).fail(function(err){
+ 		showWarningModal(err.responseJSON.message)
+ 	});
+ }
+ 
+ function isProductAlreadyAdded(productId){
+ 	productExists=false;
+ 	$(".hiddenProductId").each(function(e){
+ 		aproductId = $(this).val();
+ 		if(aproductId == productId){
+ 			productExists=true;
+ 			return;
+ 		}
+ 	});	
+ 	return productExists;
+ }
+ 
+
+ function generateProductCode(productId, productName, mainImagePath, productCost, productPrice, shippingCost){
+ 	nextCount = $(".hiddenProductId").length +1;
+ 	quantityId = "quantity"+nextCount;
+ 	priceId = "price"+nextCount;
+ 	subtotalId = "subtotal"+nextCount;
+ 	
+ 	htmlCode= `
+ 		<div class="border rounded p-1">
+				<input type="hidden" name="productId" value="${productId}" class="hiddenProductId" />
+					<div class="row">
+						<div class="col-1">
+							<div>${nextCount}</div>
+						</div>
+						<div class="col-3">
+							<img src="${mainImagePath}" class="img-fluid" />
+						</div>
+					</div>
+
+					<div class="row m-2">
+						<b>${productName}</b>
+					</div>
+
+					<div class="row m-2">
+						<table>
+							<tr>
+								<td>Product Cost:</td>
+								<td><input type="text" required
+									class="form-control m-1 cost-input" style="max-width: 140px"
+									rowNumber="${nextCount}"
+									value="${productCost}" /></td>
+							</tr>
+							<tr>
+								<td>Product quantity:</td>
+								<td><input type="number" step="1" min="1" max="5"
+									rowNumber="${nextCount}" required
+									class="form-control m-1 quantity-input"
+									style="max-width: 140px" id="${quantityId}"
+									value="1" /></td>
+							</tr>
+							<tr>
+								<td>Unit price:</td>
+								<td><input type="text" required
+									class="form-control m-1 price-input" style="max-width: 140px"
+									rowNumber="${nextCount}"
+									id="${priceId}"
+									value="${productPrice}" /></td>
+							</tr>
+							<tr>
+								<td>Subtotal:</td>
+								<td><input type="text" readonly
+									class="form-control m-1 subtotal-output"
+									id="${subtotalId}" style="max-width: 140px"
+									value="${productPrice}" /></td>
+							</tr>
+							<tr>
+								<td>Shipping Cost:</td>
+								<td><input type="text" required
+									class="form-control m-1 ship-input" style="max-width: 140px"
+									rowNumber="${nextCount}"
+									value="${shippingCost}" /></td>
+							</tr>
+						</table>
+					</div>
+				</div>
+				<div class="row">&nbsp;</div>
+ 	`;
+ 	
+ 	return htmlCode;
+ }
